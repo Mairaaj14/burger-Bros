79ae20b9994a4ec8c9ac0b099f4ad12cd62c698b
Maira Vianey Jimenez Ledesma <mvjl@Mairas-MacBook-Pro.local>
2019-01-08 12:36:47 -0800

    update file

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,18 @@
 var mysql = require("mysql");
 
 // Create a local connection to burrgers_db
-
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+   
     user: "root",
     password: "password",
     database: "burger_db"
 });
+};
 
 // Making the connection
 connection.connect(function(err) {
